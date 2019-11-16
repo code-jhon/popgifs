15b8ba6c8643df4a6005f26e96d9f28222322a1f
javilaotz <javilaortiz@gmail.com>
2019-11-16 12:39:00 -0500

    SearchResult functionalities & readme

--- src/components/lib/giphy.js
+++ src/components/lib/giphy.js
@@ -1,17 +1,10 @@
 import {api_key} from './data';
 
 export const search = (term) => {
-  let URL       = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=${api_key}`,
-        gifs_resp = '';
+  let URL       = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=${api_key}`;
 
-  fetch(URL).then(
-    response => response.json()
-  ).then(
-    (gifs) => {
-      console.log(gifs, "gifs");
-      gifs_resp = gifs;
-    }
-  )
-
-  return gifs_resp;
+  return Promise.resolve(
+    fetch(URL).then(
+      response => response.json()
+    ).then( gifs => gifs ));
 }
