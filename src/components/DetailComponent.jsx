022cdc54d4d52ff5e79f2e26f267cc21d2ce640e
javilaotz <javilaortiz@gmail.com>
2019-11-16 17:50:53 -0500

    Image Destails shows image from search results

--- src/components/DetailComponent.jsx
+++ src/components/DetailComponent.jsx
@@ -1,11 +1,19 @@
-// import React from 'react';
+import React from 'react';
+import GifPlayer from 'react-gif-player'
 import { Container, Row } from 'reactstrap';
 import { connect } from 'react-redux';
 
 const DetailComponent = (props) => {
+  console.log(props.item)
+  const item   = props && props.item,
+        // source = item && item.images.original.url,
+        source = item && item.images,
+        images = source && source.original,
+        url    = images && images.url;
+
   return (
     <div>
-
+      {url && <GifPlayer gif={url} still={url} />}
     </div>
   )
 }
