914423c7036c1586af0e1f626b39b3308e6008ae
javilaotz <javilaortiz@gmail.com>
2019-11-15 22:29:15 -0500

    Anchor left

--- src/components/ResultItemComponent.jsx
+++ src/components/ResultItemComponent.jsx
@@ -2,19 +2,26 @@ import React from 'react';
 import { Col } from 'reactstrap';
 
 const ResultItemComponent = (props) => {
-  const title  = props && props.title,
-        source = props && props.link;
+  const title   = props && props.title,
+        source  = props && props.link,
+        message = props && props.message;
 
-  return (
-    <Col>
-      <div >
-        <img className="result-item sepia" src={source} />
-        <div className="item-title">
-          {title}
-        </div>
-      </div>
-    </Col>
-  )
+  let   item    = null;
+
+  if(message){
+    item = <h1>{message}</h1>
+  }else{
+    item = <Col>
+              <div >
+                <img className="result-item sepia" src={source} />
+                <div className="item-title">
+                  {title}
+                </div>
+              </div>
+            </Col>
+  }
+
+  return item;
 }
 
 export default ResultItemComponent;
