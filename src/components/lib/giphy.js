e5091c60490066da7e733b0daeb48a7863346283
Jhon Erik <javilaortiz@gmail.com>
2023-01-22 13:25:44 -0500

    Pg 010 (#43)
    * Cleaning code
    
    * Cleaning code

--- src/components/lib/giphy.js
+++ src/components/lib/giphy.js
@@ -3,11 +3,15 @@ import {api_key} from './data';
 export const search = (term) => {
   let URL = `http://api.giphy.com/v1/gifs/search?q=${term}&api_key=${api_key}`;
 
-  return Promise.resolve(
-    fetch(URL).then(
-      response => response.json()
-    ).then( gifs => gifs ));
-}
+  fetch(URL).then(
+    response => response.json()
+  ).then(
+    (gifs) => {
+      gifs_resp = gifs;
+    }
+  )
+
+
 
 export const searchId = (id) => {
   let URL = `https://api.giphy.com/v1/gifs/${id}?api_key=${api_key}`;
